67f4343a51389f120eb0e3927ff86b50267da10b
AlexTerencio <145346170+AlexTerencio@users.noreply.github.com>
2023-09-25 17:39:28 +0200

    Main first version

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,28 @@
 Auteur : Alex Terencio
 Date de création : 25/09/2023
 Modifier :
-Description : 
+Description : Ce code prendra les informations des utilisateurs et les ecrira dans un fichier texte
 
 """
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "Entrée votre nom est prenom" << endl;
+    getline (cin,Nom_Prenom); 
+    string const nomFichier("/Profils/"Nom_Prenom);
+    ifstream monFlux(nomFichier.c_str());
+    if(monFlux)
+    {
+        string user_entry
+        cin >> user_entry;
+        monFlux << user_entry << endl;
+        string ligne;
+        while(getline(monFlux,ligne))
+        cout << ligne << endl; 
+    }
+    return 0;
+}
